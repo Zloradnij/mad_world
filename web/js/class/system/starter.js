55d865deee3dce282206e73331af827fc108c9db
Гаврышев Пётр <skifishpank@yandex.ru>
2016-12-09 14:58:03 +0700

    first

---
+++ web/js/class/system/starter.js
@@ -0,0 +1,7 @@
+redRitter = new RedRitter();
+redRitter.createPosition(2,5,5);
+
+blackRitter1 = new BlackRitter();
+blackRitter2 = new BlackRitter();
+blackRitter1.createPosition(2,7,7);
+blackRitter2.createPosition(2,7,8);
--- web/js/m.js
+++ web/js/m.js
@@ -1,12 +1,5 @@
 
 $(document).ready(function(){
-	redRitter = new RedRitter();
-	redRitter.createPosition(2,5,5);
-
-	blackRitter1 = new BlackRitter();
-	blackRitter2 = new BlackRitter();
-	blackRitter1.createPosition(2,7,7);
-	blackRitter2.createPosition(2,7,8);
 
 	redRitter.move(2,1,0);
 	blackRitter1.move(2,1,-1);
