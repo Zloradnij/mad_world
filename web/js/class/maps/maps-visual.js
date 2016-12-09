bf5503d400782cd8c91318d4dfd67dea7e2d9199
Гаврышев Пётр <skifishpank@yandex.ru>
2016-12-09 15:21:24 +0700

    first

--- web/js/class/maps/maps-visual.js
+++ web/js/class/maps/maps-visual.js
@@ -1,25 +1,60 @@
 function MapVisual(){
-    this.position = {};
     this.ID;
+
     this.sizeX;
     this.sizeY;
+
+    this.positionX;
+    this.positionY;
+
+    this.canvas;
 }
 MapVisual.prototype = {
-    setupAuto : function(ID,sizeX,sizeY){
-        this.ID = ID;
-        this.sizeX = sizeX;
-        this.sizeY = sizeY;
+    setupAuto : function(){
+        this.ID = 'mCanvas';
+
+        this.sizeX = $(document).width();
+        this.sizeY = $(document).height();
+
+        this.positionX = 0;
+        this.positionY = 0;
     },
 
-    setupHandle : function(ID,sizeX,sizeY){
+    setupHandle : function(ID,sizeX,sizeY,top,left){
         this.ID = ID;
+
         this.sizeX = sizeX;
         this.sizeY = sizeY;
+
+        this.positionX = left;
+        this.positionY = top;
     },
 
-    create : function(level,x,y){
-        this.position.x = x;
-        this.position.y = y;
+    rendering : function(){
+        $(document).append('<canvas id="'+this.ID+'"></canvas>');
+        this.canvas = $('#'+this.ID);
+        this.canvas.css({
+            width : '',
+            height : '',
+            background : '',
+            position : '',
+            top : '',
+            left : '',
+        });
+    },
+
+    setSize : function(){
+        this.canvas.css({
+            width : this.sizeX,
+            height : this.sizeY,
+        });
+    },
+
+    setPosition : function(){
+        this.canvas.css({
+            top : this.positionY,
+            left : this.positionX,
+        });
     }
-}
+};
 
