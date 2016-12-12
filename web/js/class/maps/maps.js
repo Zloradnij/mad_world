17b74dab70656e21dd5d85541bfe120c9d9f8320
Гаврышев Пётр <skifishpank@yandex.ru>
2016-12-12 14:29:35 +0700

    first

--- web/js/class/maps/maps.js
+++ web/js/class/maps/maps.js
@@ -1,11 +1,24 @@
 function Map(){
-    this.position = {};
+	this.position = {};
+	this.mapBlocks = [];
 }
 Map.prototype = {
 
 	create : function(level,x,y){
 		this.position.x = x;
 		this.position.y = y;
+
+		for (var i = 0; i < this.position.x; i++) {
+			this.mapBlocks[i] = [];
+			for (var j = 0; j < this.position.y; j++) {
+				this.mapBlocks[i][j] = Math.random() * 4;
+			}
+		}
+
+		console.log('mapBlocks = ');
+		console.log(this.mapBlocks);
 	}
-}
+
+
+};
 
